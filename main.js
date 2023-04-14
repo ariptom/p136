bec6a16f8905bdf1d372d296eb95a27c8f6566b0
ariptom <95114854+ariptom@users.noreply.github.com>
2023-04-14 19:28:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,47 @@
-function setup(){
-    video=createCapture(VIDEO);
-    video.size(550,500);
-    video.position(0,75);
-
-    canvas=createCanvas(600,450);
-    canvas.position(750,100);
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
-}
-function draw(){
-    background('#969A97');
-    fill('#F90093');
-    stroke('#F90093');
-    square(noseX,noseY,difference);
-}
-function modelLoaded(){
-    console.log('PoseNet is initialized');
-}
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results);
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
-        lwristX=results[0].pose.leftWrist.x;
-        rwristX=results[0].pose.rightWrist.x;
-        difference=floor(lwristX-rwristX);
-        console.log(noseX+" "+noseY+" "+leftWrist+" "+rwristX+" "+difference);
-        document.getElementById("side").innerHTML="side of square = "+difference;
-    }
-}
-
 noseX=0;
 noseY=0;
-difference=0;
-lwristX=0;
-rwristX=0;
-function preload(){
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
 }
